489231b509a7be296856b4a11827828d29b4f7e6
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-02-17 11:36:03 +0000

    install and config flowbite

--- cyber-reports/tailwind.config.ts
+++ cyber-reports/tailwind.config.ts
@@ -1,18 +1,43 @@
 import type { Config } from "tailwindcss";
+import flowbitePlugin from 'flowbite/plugin';
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
+      screens: {
+        'xs': '400px',
+        // => @media (min-width: 470px) { ... }
+  
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'lgg': '1057px',
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    flowbitePlugin
+  ],
 } satisfies Config;
