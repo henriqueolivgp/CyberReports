cf705c891fc0638c646100d8bea5cf023ed41436
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-02-28 21:57:33 +0000

    create navbar with all components

--- cyber-reports/src/components/navbar/navbar.tsx
+++ cyber-reports/src/components/navbar/navbar.tsx
@@ -1,12 +1,35 @@
-
+"use client";
+import { useState } from "react";
+import { Li } from "./li's/li";
+import { Logo } from "./logo/logo";
+import { MobileMenu } from "./mobile/mobile-button";
+// import { SearchInput } from "./search/search";
 
 export default function Navbar() {
-
+  const [isLoggedIn] = useState(false);
 
   return (
     <>
-      <nav>
-        
+      <nav className="flex bg-transparent w-full p-4 items-center justify-between">
+        <section className="flex gap-8">
+          <Logo />
+          <section>{/* <SearchInput /> */}</section>
+        </section>
+        <section className="hidden xl:flex">
+          <ul className=" flex gap-16 font-semibold ">
+            <Li to="/reports" name="Reports" />
+            <Li to="/explore" name="Explore" />
+            <Li to="/about-us" name="About us" />
+            {!isLoggedIn && (
+              <>
+                <Li to="/sign-up" name="Sign-up" />
+                <Li to="/sign-in" name="Sign-in" />
+              </>
+            )}
+          </ul>
+        </section>
+
+        <MobileMenu />
       </nav>
     </>
   );
