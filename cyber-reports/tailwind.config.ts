b56a43deafdf2495c6b439fe7c601e362fb2d735
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-02-28 21:55:07 +0000

    remove flowbite plugin

--- cyber-reports/tailwind.config.ts
+++ cyber-reports/tailwind.config.ts
@@ -1,34 +1,33 @@
 import type { Config } from "tailwindcss";
-import flowbitePlugin from 'flowbite/plugin';
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
       screens: {
-        'xs': '400px',
+        xs: "400px",
         // => @media (min-width: 470px) { ... }
-  
-        'sm': '640px',
+
+        sm: "640px",
         // => @media (min-width: 640px) { ... }
-  
-        'md': '768px',
+
+        md: "768px",
         // => @media (min-width: 768px) { ... }
-  
-        'lg': '1024px',
+
+        lg: "1024px",
         // => @media (min-width: 1024px) { ... }
-  
-        'lgg': '1057px',
-  
-        'xl': '1280px',
+
+        lgg: "1057px",
+
+        xl: "1280px",
         // => @media (min-width: 1280px) { ... }
-  
-        '2xl': '1536px',
+
+        "2xl": "1536px",
         // => @media (min-width: 1536px) { ... }
       },
       colors: {
@@ -37,7 +36,5 @@ export default {
       },
     },
   },
-  plugins: [
-    flowbitePlugin
-  ],
+  plugins: [],
 } satisfies Config;
