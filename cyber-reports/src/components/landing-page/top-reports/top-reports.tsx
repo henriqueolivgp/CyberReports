bbcb2cb2367fd85a80a7e3b72ea5cd8e2922d37e
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-03-03 15:45:26 +0000

    Top reports

---
+++ cyber-reports/src/components/landing-page/top-reports/top-reports.tsx
@@ -0,0 +1,216 @@
+import Image from "next/image";
+import { ReportsFilter } from "./filter/filter-reports";
+
+export const TopReports = () => {
+  // const maxReports = 8;
+
+  return (
+    <>
+      <section className="w-full flex flex-col justify-center items-center min-h-96 max-h-full bg-slate-900">
+        <ReportsFilter/>
+        <section className="grid lg:grid-cols-4 md:grid-cols-2 sm:gri-cols-2 xs:grid-cols-1 gap-8 h-full w-full p-10 ">
+          <section className="flex p-4 items-center w-96 h-24 bg-slate-800 rounded-lg mx-auto overflow-hidden">
+            <section className="flex justify-center items-center w-10 h-10 bg-white rounded-full">
+              <Image
+                src="/dedfaultUser/default-avatar.jpg"
+                width={40}
+                height={40}
+                alt="user"
+                className="rounded-full"
+              />
+            </section>
+            <section className="flex mx-2 w-[85%] h-full border-l border-l-slate-600">
+              <section className="ml-4 text-sm space-y-1 overflow-y-scroll custom-scrollbar ">
+                <p className="font-semibold">Jhon Doe </p>
+                <section className="flex ">
+                  <p className=" w-full inline-flex text-x s">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Iusto facere repudiandae ea quos quam, nobis laboriosam,
+                    provident dignissimos sed deserunt laborum. Aperiam
+                    aspernatur dolore dolorem repellendus enim, quisquam nostrum
+                    exercitationem?
+                  </p>
+                </section>
+              </section>
+            </section>
+          </section>
+          <section className="flex p-4 items-center w-96 h-24 bg-slate-800 rounded-lg mx-auto overflow-hidden">
+            <section className="flex justify-center items-center w-10 h-10 bg-white rounded-full">
+              <Image
+                src="/dedfaultUser/default-avatar.jpg"
+                width={40}
+                height={40}
+                alt="user"
+                className="rounded-full"
+              />
+            </section>
+            <section className="flex mx-2 w-[85%] h-full border-l border-l-slate-600">
+              <section className="ml-4 text-sm space-y-1 overflow-y-scroll custom-scrollbar ">
+                <p className="font-semibold">Jhon Doe </p>
+                <section className="flex ">
+                  <p className=" w-full inline-flex text-x s">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Iusto facere repudiandae ea quos quam, nobis laboriosam,
+                    provident dignissimos sed deserunt laborum. Aperiam
+                    aspernatur dolore dolorem repellendus enim, quisquam nostrum
+                    exercitationem?
+                  </p>
+                </section>
+              </section>
+            </section>
+          </section>
+          <section className="flex p-4 items-center w-96 h-24 bg-slate-800 rounded-lg mx-auto overflow-hidden">
+            <section className="flex justify-center items-center w-10 h-10 bg-white rounded-full">
+              <Image
+                src="/dedfaultUser/default-avatar.jpg"
+                width={40}
+                height={40}
+                alt="user"
+                className="rounded-full"
+              />
+            </section>
+            <section className="flex mx-2 w-[85%] h-full border-l border-l-slate-600">
+              <section className="ml-4 text-sm space-y-1 overflow-y-scroll custom-scrollbar ">
+                <p className="font-semibold">Jhon Doe </p>
+                <section className="flex ">
+                  <p className=" w-full inline-flex text-x s">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Iusto facere repudiandae ea quos quam, nobis laboriosam,
+                    provident dignissimos sed deserunt laborum. Aperiam
+                    aspernatur dolore dolorem repellendus enim, quisquam nostrum
+                    exercitationem?
+                  </p>
+                </section>
+              </section>
+            </section>
+          </section>
+          <section className="flex p-4 items-center w-96 h-24 bg-slate-800 rounded-lg mx-auto overflow-hidden">
+            <section className="flex justify-center items-center w-10 h-10 bg-white rounded-full">
+              <Image
+                src="/dedfaultUser/default-avatar.jpg"
+                width={40}
+                height={40}
+                alt="user"
+                className="rounded-full"
+              />
+            </section>
+            <section className="flex mx-2 w-[85%] h-full border-l border-l-slate-600">
+              <section className="ml-4 text-sm space-y-1 overflow-y-scroll custom-scrollbar ">
+                <p className="font-semibold">Jhon Doe </p>
+                <section className="flex ">
+                  <p className=" w-full inline-flex text-x s">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Iusto facere repudiandae ea quos quam, nobis laboriosam,
+                    provident dignissimos sed deserunt laborum. Aperiam
+                    aspernatur dolore dolorem repellendus enim, quisquam nostrum
+                    exercitationem?
+                  </p>
+                </section>
+              </section>
+            </section>
+          </section>
+          <section className="flex p-4 items-center w-96 h-24 bg-slate-800 rounded-lg mx-auto overflow-hidden">
+            <section className="flex justify-center items-center w-10 h-10 bg-white rounded-full">
+              <Image
+                src="/dedfaultUser/default-avatar.jpg"
+                width={40}
+                height={40}
+                alt="user"
+                className="rounded-full"
+              />
+            </section>
+            <section className="flex mx-2 w-[85%] h-full border-l border-l-slate-600">
+              <section className="ml-4 text-sm space-y-1 overflow-y-scroll custom-scrollbar ">
+                <p className="font-semibold">Jhon Doe </p>
+                <section className="flex ">
+                  <p className=" w-full inline-flex text-x s">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Iusto facere repudiandae ea quos quam, nobis laboriosam,
+                    provident dignissimos sed deserunt laborum. Aperiam
+                    aspernatur dolore dolorem repellendus enim, quisquam nostrum
+                    exercitationem?
+                  </p>
+                </section>
+              </section>
+            </section>
+          </section>
+          <section className="flex p-4 items-center w-96 h-24 bg-slate-800 rounded-lg mx-auto overflow-hidden">
+            <section className="flex justify-center items-center w-10 h-10 bg-white rounded-full">
+              <Image
+                src="/dedfaultUser/default-avatar.jpg"
+                width={40}
+                height={40}
+                alt="user"
+                className="rounded-full"
+              />
+            </section>
+            <section className="flex mx-2 w-[85%] h-full border-l border-l-slate-600">
+              <section className="ml-4 text-sm space-y-1 overflow-y-scroll custom-scrollbar ">
+                <p className="font-semibold">Jhon Doe </p>
+                <section className="flex ">
+                  <p className=" w-full inline-flex text-x s">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Iusto facere repudiandae ea quos quam, nobis laboriosam,
+                    provident dignissimos sed deserunt laborum. Aperiam
+                    aspernatur dolore dolorem repellendus enim, quisquam nostrum
+                    exercitationem?
+                  </p>
+                </section>
+              </section>
+            </section>
+          </section>
+          <section className="flex p-4 items-center w-96 h-24 bg-slate-800 rounded-lg mx-auto overflow-hidden">
+            <section className="flex justify-center items-center w-10 h-10 bg-white rounded-full">
+              <Image
+                src="/dedfaultUser/default-avatar.jpg"
+                width={40}
+                height={40}
+                alt="user"
+                className="rounded-full"
+              />
+            </section>
+            <section className="flex mx-2 w-[85%] h-full border-l border-l-slate-600">
+              <section className="ml-4 text-sm space-y-1 overflow-y-scroll custom-scrollbar ">
+                <p className="font-semibold">Jhon Doe </p>
+                <section className="flex ">
+                  <p className=" w-full inline-flex text-x s">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Iusto facere repudiandae ea quos quam, nobis laboriosam,
+                    provident dignissimos sed deserunt laborum. Aperiam
+                    aspernatur dolore dolorem repellendus enim, quisquam nostrum
+                    exercitationem?
+                  </p>
+                </section>
+              </section>
+            </section>
+          </section>
+          <section className="flex p-4 items-center w-96 h-24 bg-slate-800 rounded-lg mx-auto overflow-hidden">
+            <section className="flex justify-center items-center w-10 h-10 bg-white rounded-full">
+              <Image
+                src="/dedfaultUser/default-avatar.jpg"
+                width={40}
+                height={40}
+                alt="user"
+                className="rounded-full"
+              />
+            </section>
+            <section className="flex mx-2 w-[85%] h-full border-l border-l-slate-600">
+              <section className="ml-4 text-sm space-y-1 overflow-y-scroll custom-scrollbar ">
+                <p className="font-semibold">Jhon Doe </p>
+                <section className="flex ">
+                  <p className=" w-full inline-flex text-x s">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Iusto facere repudiandae ea quos quam, nobis laboriosam,
+                    provident dignissimos sed deserunt laborum. Aperiam
+                    aspernatur dolore dolorem repellendus enim, quisquam nostrum
+                    exercitationem?
+                  </p>
+                </section>
+              </section>
+            </section>
+          </section>
+        </section>
+      </section>
+    </>
+  );
+};
