3b5335d8169d2ec58ba6b3f6e49673190c0bd4fd
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-03-03 15:47:01 +0000

    create explore page and add all components like search, sort by, filterComponent and Reports

--- cyber-reports/src/app/(public)/explore/page.tsx
+++ cyber-reports/src/app/(public)/explore/page.tsx
@@ -1,7 +1,30 @@
-export default function Explore(){
-  return(
+import FiltersComponent from "@/components/explore/filter/filter-reports";
+import { Reports } from "@/components/explore/reports/reports";
+import { Search } from "@/components/explore/search/search";
+import { SortBy } from "@/components/explore/sort-by/sort-by";
+
+export default function Explore() {
+  return (
     <>
-    <h1>Explore</h1>
+      <section className="flex flex-col justify-center min-h-screen max-h-full mx-auto ">
+        <section className="flex justify-between gap-2 p-8 px-56">
+          <Search />
+          <SortBy />
+          <FiltersComponent  />
+        </section>
+        <section className="flex justify-center w-full h-full">
+          <section className="grid lg:grid-cols-4 md:grid-cols-3 xs:grid-cols-1  gap-y-8">
+            <Reports />
+            <Reports />
+            <Reports />
+            <Reports />
+            <Reports />
+            <Reports />
+            <Reports />
+            <Reports />
+          </section>
+        </section>
+      </section>
     </>
-  )
+  );
 }
