3b4b51804f479bc811d6cf09507cb7bd6198c66e
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-03-18 15:22:14 +0000

    add a eslint ignore into nextconfig file

--- cyber-reports/next.config.ts
+++ cyber-reports/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ["i.ibb.co","source.unsplash.com"],
     remotePatterns: [
