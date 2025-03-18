a317252da3c35a1a5b214e1a3ffb7ad86e39c410
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-03-18 16:13:18 +0000

    change path with out

--- cyber-reports/next.config.ts
+++ cyber-reports/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  distDir: 'cyber-reports/.vercel/output',
+  distDir: 'out',
   eslint: {
     ignoreDuringBuilds: true,
   },
