91c6cc76303ed156f731fd74092c172ce4b6598f
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-03-31 12:06:29 +0100

    create a profile page and your layout with sideBar

---
+++ cyber-reports/src/app/(private)/profile/layout.tsx
@@ -0,0 +1,89 @@
+"use client";
+import "@/styles/globals.css";
+import SideBar from "@/components/profile/sidebar/sidebar";
+import { Footer } from "@/components/footer/footer";
+import Navbar from "@/components/navbar/navbar";
+import { ProfileLi } from "@/components/profile/sidebar/profile-li";
+import { usePathname } from "next/navigation";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const pathname = usePathname();
+  return (
+    <>
+      <Navbar />
+      <SideBar>
+        <section className="">
+          <ProfileLi
+            to="/profile"
+            name="Profile"
+            activeTo={pathname}
+            activeLocal={pathname}
+            svg={"profile"}
+          />
+          <ProfileLi
+            to="/profile/edit-profile"
+            name="Edit-Profile"
+            activeTo={pathname}
+            activeLocal={pathname}
+            svg={"editprofile"}
+          />
+          <ProfileLi
+            to="/profile/reports"
+            name="Reports"
+            activeTo={pathname}
+            activeLocal={pathname}
+            svg={"reports"}
+          />
+          <ProfileLi
+            to="/profile/chat"
+            name="Chat"
+            activeTo={pathname}
+            activeLocal={pathname}
+            svg={"chat"}
+          />
+          <ProfileLi
+            to="/profile/historic"
+            name="Historic Reports"
+            activeTo={pathname}
+            activeLocal={pathname}
+            svg={"userhistoric"}
+          />
+          {/* {isSpecialist && (
+                            <ProfileLi to="/profile/all-reports" name="All Reports" activeTo={pathname} activeLocal={pathname} svg={'chat'} />
+                            )}
+                            {isAdmin && (
+                                <ProfileLi to="/profile/admin" name="Admin Control" activeTo={pathname} activeLocal={pathname} svg={'admin'} />
+                            )} */}
+          <ProfileLi
+            to="/profile/settings"
+            name="Settings"
+            activeTo={pathname}
+            activeLocal={pathname}
+            svg={"settings"}
+          />
+        </section>
+        <section className="w-full">
+          <ProfileLi
+            to="#"
+            name="logout"
+            activeTo={pathname}
+            activeLocal={pathname}
+            svg={"logout"}
+          />
+        </section>
+      </SideBar>
+      <section className="flex-1">
+        <section className="p-4 sm:ml-64 w-[calc(100%-16rem)]">
+          {children}
+        </section>
+        <section className=" bottom-0 p-0 sm:ml-64 w-[calc(100%-16rem)]">
+          <Footer />
+        </section>
+      </section>
+    </>
+  );
+}
--- cyber-reports/src/app/(private)/profile/page.tsx
+++ cyber-reports/src/app/(private)/profile/page.tsx
@@ -1,7 +1,87 @@
-export default function Profile(){
-  return(
+import Image from "next/image";
+
+export default function Profile() {
+  return (
     <>
-    <h1>Profile</h1>
+      <section className="flex-wrap h-screen">
+        <section className="flex flex-col ">
+          <section className="banner bg-violet-100 rounded-t-lg h-48 w-full">
+            <section className="Text-Button flex flex-col relative z-10 xl:flex-row">
+              <section className="w-full absolute overflow-hidden h-48 rounded-t-lg xl:relative flex items-center content-center">
+                <Image
+                  className="w-screen h-full object-cover"
+                  src="https://t4.ftcdn.net/jpg/05/52/98/77/360_F_552987749_4Y5SJa4KRL2UIzVrk5vznfbQtDeJZtqe.jpg"
+                  fill
+                  alt="banner"
+                />
+                <span className="absolute text-white text-5xl font-bold flex items-center justify-center w-full h-full">
+                  User Banner
+                </span>
+
+                {/* <section>
+                  <Image
+                    className="w-screen h-full object-cover"
+                    fill
+                    src=""
+                    alt="User Banner"
+                  />
+                </section> */}
+              </section>
+            </section>
+          </section>
+
+          <section className="w-full min-h-32 bg-white border border-gray-200 shadow dark:bg-gray-800 dark:border-gray-700">
+            <section className="flex justify-end px-4 pt-4"></section>
+            <section className="flex items-center">
+              <section className="flex w-full gap-4">
+                <section className="relative">
+                  <section className="w-24 h-24 object-cover">
+                    <Image
+                      className="w-24 h-24 mb-3 ml-4 rounded-full  shadow-2xl"
+                      src="/defaultUser/user.png"
+                      fill
+                      alt="User Avatar"
+                    />
+                  </section>
+                  <span className="absolute bottom-3 left-5 w-6 h-6 border-white dark:border-gray-800 rounded-full">
+                    <Image src="/UserVerified.png" fill alt="userVerified" />
+                  </span>
+                </section>
+
+                {/* <section className="relative">
+                <Image
+                className="w-24 h-24 mb-3 ml-4 rounded-full shadow-2xl"
+                src="/defaultUser/user.png"
+                fill
+                alt="User Avatar"
+                />
+                <span className="absolute bottom- w-6 h-6 border-white dark:border-gray-800 rounded-full">
+                <Image src="/UserVerified.png" fill alt="userVerified" />
+                </span>
+                </section> */}
+
+                <section className="flex flex-col ml-4">
+                  <h5 className="mb-1 text-3xl font-medium text-gray-900 dark:text-white">
+                    User Name
+                  </h5>
+                  <p className=" text-sm">Date Joined :12/02/2000</p>
+                  <p className=" text-sm">Las Login: 12/02/2000</p>
+                </section>
+                <section className="px-4"></section>
+                <section className="flex flex-1 p-4">
+                  <section className="text-sm text-gray-500 dark:text-gray-400">
+                    <span>Total de Denúncias</span>
+                    <br />
+                    <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                      237
+                    </h5>
+                  </section>
+                </section>
+              </section>
+            </section>
+          </section>
+        </section>
+      </section>
     </>
-  )
+  );
 }
