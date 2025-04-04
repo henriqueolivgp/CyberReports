9a0282002ba16075e9de4a40c23ef001a5a9a87d
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-04-04 11:10:04 +0100

    Register user in auth service

---
+++ API/src/services/auth.service.ts
@@ -0,0 +1,48 @@
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
+import { UserModel } from "../models/user.model";
+import { uploadImageToR2 } from "../utils/uploadToR2";
+
+export const AuthService = {
+  async registerUser(
+    full_name: string,
+    username: string,
+    email: string,
+    password: string,
+    avatar: { buffer: Buffer; filename: string }
+  ) {
+    // Verificando se o usuário já existe
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser) {
+      throw new Error("Usuário já registrado");
+    }
+
+    // Hash da senha antes de salvar no MongoDB
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Criando usuário no MongoDB
+    const newUser = new UserModel({
+      full_name,
+      username,
+      email,
+      password: hashedPassword,
+      avatarUrl: '', // Corrigido para avatarUrl conforme o schema
+    });
+
+    // Usar o ID do usuário para nomear o arquivo no R2
+    const userId = newUser._id.toString(); // Obter o ID do usuário como string
+    const fileExtension = avatar.filename.split(".").pop(); // Obter a extensão do arquivo (ex.: "png")
+    const uniqueFileName = `${userId}.${fileExtension}`; // Nome do arquivo será o ID do usuário + extensão
+
+    // Fazer o upload da imagem para o Cloudflare R2 usando o ID do usuário
+    const avatarUrl = await uploadImageToR2(avatar.buffer, uniqueFileName);
+
+    // Atualizar o usuário com a URL do avatar
+    newUser.avatarUrl = avatarUrl;
+    await newUser.save();
+
+    await newUser.save();
+
+    return newUser;
+  },
+};
