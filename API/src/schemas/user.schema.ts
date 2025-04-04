593a3f80f960c226c0338927d11d097d559c6367
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-04-04 11:12:21 +0100

    create a UserSchema with moongose

---
+++ API/src/schemas/user.schema.ts
@@ -0,0 +1,32 @@
+import { Schema } from 'mongoose';
+
+const UserSchema = new Schema(
+  {
+    full_name: { type: String, required: true, trim: false },
+    username: { type: String, required: true, trim: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, 'Por favor insira um email válido.'],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: [6, 'A senha deve ter pelo menos 6 caracteres.'],
+    },
+    avatarUrl: {
+      // Alterado de "avatar" para "avatarUrl" para consistência
+      type: String,
+      required: false,
+      default: '/src/static/user.png',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export { UserSchema };
