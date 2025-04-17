c664656b1a032bd3d59466f3ae0da53943de043c
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-04-17 10:34:39 +0100

    create more validations for user in this case for login and get profile

--- API/src/validations/user.validation.ts
+++ API/src/validations/user.validation.ts
@@ -1,23 +1,33 @@
 import { z } from "zod";
 
 // Esquema de validação com zod
-export const RegisterValidationSchema = z.object({
+const RegisterValidationSchema = z.object({
   full_name: z.string().min(1, 'Nome completo é obrigatório'),
   username: z.string().min(1, 'Nome de usuário é obrigatório'),
   email: z.string().email('Email inválido'),
   password: z.string().min(6, 'A senha deve ter pelo menos 6 caracteres'),
-  avatar: z.string().optional(), // Opcional, como no Mongoose
 });
 
+type Register = z.infer<typeof RegisterValidationSchema>;
+
 const LoginValidationSchema = z.object({
-  id: z.number().optional(),
   email: z.string().email('Email inválido'),
   password: z.string().min(6, 'A senha deve ter pelo menos 6 caracteres'),
 });
 
 type Login = z.infer<typeof LoginValidationSchema>;
 
+const GetUserProfileValidationSchema = z.object({
+  id: z.string()
+});
+
+type Profile = z.infer<typeof GetUserProfileValidationSchema>;
+
 export {
+  RegisterValidationSchema,
+  Register,
   LoginValidationSchema,
-  Login
+  Login,
+  GetUserProfileValidationSchema,
+  Profile
 }
