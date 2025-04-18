ee1826bad67d302c7d00748aa08c061f62678470
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-04-18 17:45:22 +0100

    remove mongo into mongoose import, add dotenv configuration and remove url string in API

--- API/src/db/connection.ts
+++ API/src/db/connection.ts
@@ -1,13 +1,25 @@
-import mongoose, { mongo } from 'mongoose'
+import mongoose from 'mongoose';
+// DotEnv configuration
+import * as dotenv from 'dotenv';
+dotenv.config({ path: './.env'});
 
-const DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://henriqueoliveiragp:6MDZKKeKTqBOZZWS@cyber-reports.xquezxt.mongodb.net/CyberReports?retryWrites=true&w=majority&appName=cyber-reports";
+const DATABASE_URL = process.env.DATABASE_URL;
 
-export async function ConnectionDB(){
+// ConnectionDB Function
+export async function ConnectionDB() {
+
+  // If doesn't have a value inside the variable throw a error
+  if (!DATABASE_URL) {
+    console.log("❌ DATABASE_URL não está definida no .env");
+    process.exit(1);
+  }
+
   try {
-    await mongoose.connect(DATABASE_URL!);
-    console.log('Connected with MongoDB')
+    // waits mongoose to connect into DB
+    await mongoose.connect(DATABASE_URL);
+    console.log('Connected with MongoDB');
   } catch (error) {
-    console.log("❌ Error when connect to MongoDB");
-    process.exit(1)
+    console.log("❌ Error when connect to MongoDB:", error); // throw the error
+    process.exit(1);
   }
 }
